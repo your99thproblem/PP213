4f58738bb6e2be2d1a455ea38f5e7c83c9af0c1c
dshkalikov <bern1991@gmail.com>
2022-05-31 15:19:10 +0300

    31052022

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -16,7 +16,7 @@ public class AnimalsCage {
     private Animal animal;
     private Timer timer;
     @Autowired
-    public AnimalsCage(@Qualifier("cat") Animal animal, Timer timer) {
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
         this.animal = animal;
         this.timer = timer;
     }
