09a1cb8a06a6cadc6abd5c60b0822849568778a9
dshkalikov <bern1991@gmail.com>
2022-05-31 15:18:27 +0300

    31052022

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
-@Bean
     public Long getTime() {
         return nanoTime;
     }
